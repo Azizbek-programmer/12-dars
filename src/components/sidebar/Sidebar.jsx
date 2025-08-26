ee322d371df49d9d66d85782a7b21fae194d1a18
Azizbek Programmer <azizbekmirzavaliyev31@gmail.com>
2025-08-26 13:30:05 +0500

    ==========[2]

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,8 @@ import "./Sidebar.scss"
 
 const Sidebar = () => {
   const [name, setName] = useState("")
+  const [lastname, setLastName] = useState("")
+  const [job, setjob] = useState("")
   const [age, setAge] = useState("")
   const [data, setData] = useState(JSON.parse(localStorage.getItem("sidebar")) || [])
 
@@ -14,11 +16,15 @@ const Sidebar = () => {
     event.preventDefault()
     let pet = {
       name,
+      lastname,
+      job,
       age: Number(age),
       id: Date.now()
     }
     setData((prev) => [...prev, pet])
     setName("")
+    setLastName("")  
+    setjob("")  
     setAge("")
   }
 
@@ -32,6 +38,8 @@ const Sidebar = () => {
         <h2>Users</h2>
         <form onSubmit={handleSubmit} className='form'>
           <input required value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='name' />
+          <input required value={lastname} onChange={(e) => setLastName(e.target.value)} type="text" placeholder='lastname' />
+          <input required value={job} onChange={(e) => setjob(e.target.value)} type="text" placeholder='job' />
           <input required value={age} onChange={(e) => setAge(e.target.value)} type="number" placeholder='age' />
           <button type="submit">Submit</button>
         </form>
@@ -41,9 +49,11 @@ const Sidebar = () => {
         {
           data.length ?
             data.map((item) => (
-              <div className="card" key={item.id}>
+              <div className="card" key={item.id}>  
                 <div className="avatar"></div>
-                <h3>{item.name}</h3>
+                <h3>name: {item.name}</h3>
+                <h5>lastname: {item.lastname}</h5>
+                <h5>job: {item.job}</h5>
                 <p>Age: {item.age}</p>
                 <div className="actions">
                   <button onClick={() => handleDelete(item.id)}>delete</button>
